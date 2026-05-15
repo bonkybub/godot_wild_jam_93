88d9c2ac1c9f53338ea1e1fbb084cb92006b3685
Cpt-Baker <Baaqirammar@gmail.com>
2026-05-15 14:28:37 -0400

    Turning Player into scene
    ~ Turned PlayerMover into a scene to test out scene transition and signal stuff

--- godot-wild-jam-93/Scripts/Character/player_mover.gd
+++ godot-wild-jam-93/Scripts/Character/player_mover.gd
@@ -5,3 +5,8 @@ var move_speed: float = 5.0
 
 func _physics_process(delta: float) -> void:
 	position.z -= move_speed * delta
+
+
+func _on_speed_upgrade_pressed() -> void:
+	
+	pass # Replace with function body.
